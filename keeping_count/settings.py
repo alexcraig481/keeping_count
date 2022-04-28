2a0d806b88bbf5475dbeb6ed1272f9629dd1b0cf
alexcraig481 <acfrady@my.waketech.edu>
2022-04-27 22:48:54 -0400

    1.Added redirect for logout to "home" ->  index.html 2. Moved base and index to new templates dir in keeping_count dir - refactored 3. Deleted logout template  - not needed though I did not see if the template would be "seen" if I put it in a registration dir in the templates dir in the keeping_count dir - maybe try this at later date.
    tested

--- keeping_count/settings.py
+++ keeping_count/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'keeping_count.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / "keeping_count" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_URL = 'users:login'
+LOGOUT_REDIRECT_URL = 'home'
